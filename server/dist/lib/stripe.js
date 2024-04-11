cbaa0c7d3140e0093b808378d0f09cf3565d3d2f
Omkar Gade <g.omkar1110@gmail.com>
2024-04-11 18:42:22 +0530

    import stripe statement

--- server/dist/lib/stripe.js
+++ server/dist/lib/stripe.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const stripe_1 = require("stripe");
+const stripe_1 = __importDefault(require("stripe"));
 require('dotenv').config;
 exports.stripe = void 0;
 exports.stripe = new stripe_1.default(process.env.STRIPE_API_KEY, {
