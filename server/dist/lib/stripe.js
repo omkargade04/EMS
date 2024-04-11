0e9df500a8ac048bc730f302ad037fa1f911d31b
Omkar Gade <g.omkar1110@gmail.com>
2024-04-11 18:27:57 +0530

    fixed hare krishn

--- server/dist/lib/stripe.js
+++ server/dist/lib/stripe.js
@@ -3,9 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.stripe = void 0;
 const stripe_1 = __importDefault(require("stripe"));
 require('dotenv').config;
+exports.stripe = void 0;
 exports.stripe = new stripe_1.default(process.env.STRIPE_API_KEY, {
     apiVersion: "2023-10-16",
     typescript: true,
