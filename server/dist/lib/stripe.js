5dc1d1648aba232ae547365241d2b4c649ca0b6e
Omkar Gade <g.omkar1110@gmail.com>
2024-04-11 18:24:59 +0530

    final stripe commit on god

--- server/dist/lib/stripe.js
+++ server/dist/lib/stripe.js
@@ -4,9 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.stripe = void 0;
-const stripe_1 = __importDefault(require("stripe"));
+const stripe = __importDefault(require("stripe"));
 require('dotenv').config;
-exports.stripe = new stripe_1.default(process.env.STRIPE_API_KEY, {
+export const stripe_1 = new stripe.default(process.env.STRIPE_API_KEY, {
     apiVersion: "2023-10-16",
     typescript: true,
 });
